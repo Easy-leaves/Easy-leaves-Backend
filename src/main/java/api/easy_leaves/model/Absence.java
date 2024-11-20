191f26d43d2f95f8d1265237c8daaed8ad31db72
LE LANNIER <lelannier.nicolas@gmail.com>
2024-11-20 17:01:29 +0100

    Mise en place de chaque variable pour chaque model

--- src/main/java/api/easy_leaves/model/Absence.java
+++ src/main/java/api/easy_leaves/model/Absence.java
@@ -3,9 +3,138 @@
  */
 package api.easy_leaves.model;
 
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 /** 
  * @author Nicolas LE LANNIER
  */
+@Entity
+@Table(name = "ABSENCE")
 public class Absence {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idAbsence;
+	
+	private Date dateDebut;
+	private Date dateFin;
+	private int type;
+	private int statut;
+	private String motif;
+	
+	
+	/** Constructeur
+	 * @param dateDebut
+	 * @param dateFin
+	 * @param type
+	 * @param statut
+	 * @param motif
+	 */
+	public Absence(Date dateDebut, Date dateFin, int type, int statut, String motif) {
+		super();
+		this.dateDebut = dateDebut;
+		this.dateFin = dateFin;
+		this.type = type;
+		this.statut = statut;
+		this.motif = motif;
+	}
+
+	/** Constructeur
+	 * 
+	 */
+	public Absence() {
+		super();
+	}
+
+	/** Getter
+	 * @return the idAbsence
+	 */
+	public int getIdAbsence() {
+		return idAbsence;
+	}
+
+	/** Setter
+	 * @param idAbsence the idAbsence to set
+	 */
+	public void setIdAbsence(int idAbsence) {
+		this.idAbsence = idAbsence;
+	}
+
+	/** Getter
+	 * @return the dateDebut
+	 */
+	public Date getDateDebut() {
+		return dateDebut;
+	}
+
+	/** Setter
+	 * @param dateDebut the dateDebut to set
+	 */
+	public void setDateDebut(Date dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+
+	/** Getter
+	 * @return the dateFin
+	 */
+	public Date getDateFin() {
+		return dateFin;
+	}
+
+	/** Setter
+	 * @param dateFin the dateFin to set
+	 */
+	public void setDateFin(Date dateFin) {
+		this.dateFin = dateFin;
+	}
+
+	/** Getter
+	 * @return the type
+	 */
+	public int getType() {
+		return type;
+	}
+
+	/** Setter
+	 * @param type the type to set
+	 */
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	/** Getter
+	 * @return the statut
+	 */
+	public int getStatut() {
+		return statut;
+	}
+
+	/** Setter
+	 * @param statut the statut to set
+	 */
+	public void setStatut(int statut) {
+		this.statut = statut;
+	}
+
+	/** Getter
+	 * @return the motif
+	 */
+	public String getMotif() {
+		return motif;
+	}
+
+	/** Setter
+	 * @param motif the motif to set
+	 */
+	public void setMotif(String motif) {
+		this.motif = motif;
+	}
+	
+	
 }
--- src/main/java/api/easy_leaves/model/Departement.java
+++ src/main/java/api/easy_leaves/model/Departement.java
@@ -3,9 +3,67 @@
  */
 package api.easy_leaves.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 /** 
  * @author Nicolas LE LANNIER
  */
+@Entity
+@Table(name = "DEPARTEMENT")
 public class Departement {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idDepartement;
+	
+	private String libelle;
+
+	/** Constructeur
+	 * @param libelle
+	 */
+	public Departement(String libelle) {
+		super();
+		this.libelle = libelle;
+	}
+
+	/** Constructeur
+	 * 
+	 */
+	public Departement() {
+		super();
+	}
+
+	/** Getter
+	 * @return the idDepartement
+	 */
+	public int getIdDepartement() {
+		return idDepartement;
+	}
+
+	/** Setter
+	 * @param idDepartement the idDepartement to set
+	 */
+	public void setIdDepartement(int idDepartement) {
+		this.idDepartement = idDepartement;
+	}
+
+	/** Getter
+	 * @return the libelle
+	 */
+	public String getLibelle() {
+		return libelle;
+	}
+
+	/** Setter
+	 * @param libelle the libelle to set
+	 */
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	
+	
 }
