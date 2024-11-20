327d507d0571d8f8189f240477b7fc2a58acf468
Theo Le Moal <contact@theo-le-moal.fr>
2024-11-20 14:28:42 +0100

    init

---
+++ src/main/java/api/easy_leaves/EasyLeavesApplication.java
@@ -0,0 +1,13 @@
+package api.easy_leaves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyLeavesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyLeavesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/easy_leaves/EasyLeavesApplicationTests.java
@@ -0,0 +1,13 @@
+package api.easy_leaves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyLeavesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
