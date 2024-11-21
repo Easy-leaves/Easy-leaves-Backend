3e836df4e3998f5adad751aeff256ed812fac23d
Josh-Newman-7 <raphael.pinson.professional@gmail.com>
2024-11-21 15:43:20 +0100

    Create Data Transfer Objects

---
+++ src/main/java/api/easy_leaves/dto/CompteurDTO.java
@@ -0,0 +1,111 @@
+package api.easy_leaves.dto;
+
+import api.easy_leaves.enums.TypeCompteur;
+
+/**
+ * Class which defines a compteur. 
+ */
+public class CompteurDTO {
+	
+	/**
+	 * The identifier.
+	 */
+    private int id;
+	
+	/**
+	 * Get the year of the compteur.
+	 */
+	private int annee;
+	
+	/**
+	 * The type of the compteur.
+	 */
+	private TypeCompteur type;
+	
+	/** 
+	 * The user identifier linked to this compteur.
+	 */
+	private int utilisateurId;
+	
+	/** 
+	 * Constructor
+	 * @param annee
+	 * @param typeCompteur
+	 * @param utilisateur
+	 */
+	public CompteurDTO(int annee, TypeCompteur type, int utilisateurId) {
+		this.annee = annee;
+		this.type = type;
+		this.utilisateurId = utilisateurId;
+	}
+	
+	/** 
+	 * Constructor.
+	 */
+	public CompteurDTO() {
+	}
+
+	/**
+	 * Getter
+	 * @return the compteur's identifier.
+	 */
+	public int getIdCompteur() {
+		return id;
+	}
+
+	/**
+	 * Setter
+	 * @param idCompteur the idCompteur to set.
+	 */
+	public void setIdCompteur(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * Getter
+	 * @return the year of the compteur.
+	 */
+	public int getAnnee() {
+		return annee;
+	}
+
+	/**
+	 * Setter
+	 * @param annee the year to set.
+	 */
+	public void setAnnee(int annee) {
+		this.annee = annee;
+	}
+
+	/**
+	 * Getter
+	 * @return the type of the compteur.
+	 */
+	public TypeCompteur getTypeCompteur() {
+		return type;
+	}
+
+	/**
+	 * Setter
+	 * @param TypeCompteur the type to set.
+	 */
+	public void setTypeCompteur(TypeCompteur type) {
+		this.type = type;
+	}
+
+	/**
+	 * Getter
+	 * @return the utilisateur id.
+	 */
+	public int getUtilisateur() {
+		return utilisateurId;
+	}
+
+	/**
+	 * Setter
+	 * @param utilisateur the utilisateur to set.
+	 */
+	public void setUtilisateur(int utilisateurId) {
+		this.utilisateurId = utilisateurId;
+	}
+}
---
+++ src/main/java/api/easy_leaves/dto/DepartementDTO.java
@@ -0,0 +1,89 @@
+package api.easy_leaves.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Class which represent a departement.
+ */
+public class DepartementDTO {
+
+	/** 
+	 * The departement identifier. 
+	 */
+    private int id;
+	
+	/** 
+	 * The libelle.
+	 */
+	private String libelle;
+	
+	/**
+	 * The list of user ids linked to this departement. 
+	 */
+    private List<Integer> utilisateurIds = new ArrayList<Integer>();
+
+	/** 
+	 * Constructor
+	 * @param libelle
+	 */
+	public DepartementDTO(String libelle) {
+		super();
+		this.libelle = libelle;
+	}
+
+	/**
+	 * Constructor
+	 */
+	public DepartementDTO() {
+		super();
+	}
+
+	/**
+	 * Getter
+	 * @return the departement identifier.
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * Setter
+	 * @param id the departement identifier to set.
+	 */
+	public void setIdDepartement(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * Getter
+	 * @return the libelle.
+	 */
+	public String getLibelle() {
+		return libelle;
+	}
+
+	/**
+	 * Setter
+	 * @param libelle the libelle to set.
+	 */
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	/**
+	 * Getter
+	 * @return the list of user ids linked to this departement.
+	 */
+	public List<Integer> getUtilisateurIds() {
+		return utilisateurIds;
+	}
+
+	/**
+	 * Setter
+	 * @param utilisateurs the utilisateurs to set.
+	 */
+	public void setUtilisateurIds(List<Integer> utilisateurIds) {
+		this.utilisateurIds = utilisateurIds;
+	}
+}
