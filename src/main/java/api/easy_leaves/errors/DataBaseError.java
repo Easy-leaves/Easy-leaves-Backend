8393ca69c1295ba8b4742ba170d16aa0f81ed70d
Josh-Newman-7 <raphael.pinson.professional@gmail.com>
2024-11-25 14:39:15 +0100

    Fix javadoc

--- src/main/java/api/easy_leaves/errors/DataBaseError.java
+++ src/main/java/api/easy_leaves/errors/DataBaseError.java
@@ -1,6 +1,20 @@
 package api.easy_leaves.errors;
 
+/**
+ * Classe représentant une exception levée lorsqu'une erreur survient lors d'une interaction avec la base de données.
+ * 
+ * Cette exception est utilisée pour signaler des problèmes liés à l'accès ou à la manipulation des données. 
+ * Elle hérite de {@link RuntimeException}.
+ *
+ * @see RuntimeException
+ */
 public class DataBaseError extends RuntimeException{
+	
+    /**
+     * Constructeur.
+     * 
+     * @param message le message détaillant l'erreur survenue
+     */
 	public DataBaseError(String message) {
 		super(message);
 	}
--- src/main/java/api/easy_leaves/errors/IncoherenceDateError.java
+++ src/main/java/api/easy_leaves/errors/IncoherenceDateError.java
@@ -1,6 +1,20 @@
 package api.easy_leaves.errors;
 
+/**
+ * Class représentant une exception levée lorsqu'une incohérence est détectée dans les dates fournies.
+ * 
+ * Cette exception est utilisée pour signaler des erreurs liées à des dates invalides
+ * ou incohérentes, comme une date de début postérieure à une date de fin.
+ *
+ * @see RuntimeException
+ */
 public class IncoherenceDateError extends RuntimeException{
+	
+    /**
+     * Constructeur.
+     * 
+     * @param message le message décrivant l'erreur d'incohérence de date.
+     */
 	public IncoherenceDateError(String message) {
 		super(message);
 	}
