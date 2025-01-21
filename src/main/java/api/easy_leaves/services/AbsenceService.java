e2e0d1c15f19d5b4ea2e7b99e825390e1d8e4c21
LE LANNIER <lelannier.nicolas@gmail.com>
2025-01-21 16:42:38 +0100

    Auto stash before merge of "dev" and "origin/dev"

--- src/main/java/api/easy_leaves/services/AbsenceService.java
+++ src/main/java/api/easy_leaves/services/AbsenceService.java
@@ -130,7 +130,12 @@ public class AbsenceService {
 		return absenceRepository.countByUtilisateurAndStatut(utilisateur, statut);
 	}
 	
+<<<<<<< Updated upstream
     public List<AbsenceDTO> getAbsencesByUtilisateurId(int utilisateurId) {
+=======
+	
+	public List<AbsenceDTO> getAbsencesByUtilisateurId(int utilisateurId) {
+>>>>>>> Stashed changes
         return absenceRepository.findByUtilisateurIdUtilisateur(utilisateurId)
                 .stream()
                 .map(AbsenceDTO::convertToDTO)
