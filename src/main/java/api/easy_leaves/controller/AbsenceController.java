1d011ce825d5bb1149ebb117be301819fa0e2d34
Driss <drissgazzaoui1986@hotmail.com>
2025-01-21 16:43:53 +0100

    resolution du Cors+ ajout methode obtenirAbsencesParUtilisateur

--- src/main/java/api/easy_leaves/controller/AbsenceController.java
+++ src/main/java/api/easy_leaves/controller/AbsenceController.java
@@ -174,4 +174,19 @@ public class AbsenceController {
     public List<AbsenceDTO> getAbsencesByUtilisateurId(@PathVariable int id) {
         return absenceService.getAbsencesByUtilisateurId(id);
     }
+	
+	/**
+	 * Récupérer toutes les absences d'un utilisateur donné.
+	 * localhost:8080/absences/utilisateur/{utilisateurId}
+	 * @param userId Identifiant de l'utilisateur
+	 * @return Liste des absences de cet utilisateur
+	 */
+	@GetMapping("/utilisateur/{utilisateurId}")
+	public List<AbsenceDTO> obtenirAbsencesParUtilisateur(@PathVariable("utilisateurId") int utilisateurId) {
+	    return absenceService.getAbsencesByUser(utilisateurId).stream()
+	            .map(AbsenceDTO::convertToDTO)
+	            .collect(Collectors.toList());
+	}
+
+	
 }
