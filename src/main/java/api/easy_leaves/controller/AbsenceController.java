ccf090907ea3dde4366060138913f4be7dafc85b
Driss <drissgazzaoui1986@hotmail.com>
2025-01-24 09:45:14 +0100

    ajout creat absence

--- src/main/java/api/easy_leaves/controller/AbsenceController.java
+++ src/main/java/api/easy_leaves/controller/AbsenceController.java
@@ -175,18 +175,6 @@ public class AbsenceController {
         return absenceService.getAbsencesByUtilisateurId(id);
     }
 	
-	/**
-	 * Récupérer toutes les absences d'un utilisateur donné.
-	 * localhost:8080/absences/utilisateur/{utilisateurId}
-	 * @param userId Identifiant de l'utilisateur
-	 * @return Liste des absences de cet utilisateur
-	 */
-	@GetMapping("/utilisateur/{utilisateurId}")
-	public List<AbsenceDTO> obtenirAbsencesParUtilisateur(@PathVariable("utilisateurId") int utilisateurId) {
-	    return absenceService.getAbsencesByUser(utilisateurId).stream()
-	            .map(AbsenceDTO::convertToDTO)
-	            .collect(Collectors.toList());
-	}
 
 	
 }
